f395dbbe05b48958366bb4355c97722be333f78b
walcsalot <walcsalot@gmail.com>
2025-10-27 04:23:12 +0800

    Initial commit

---
+++ Scenes/card_ui/card_states/card_released_state.gd
@@ -0,0 +1,19 @@
+extends CardState
+
+var played: bool
+
+func enter() -> void:
+	card_ui.color.color = Color.BLACK
+	card_ui.state.text = "RELEASED"
+	
+	played = false
+	
+	if not card_ui.targets.is_empty():
+		played = true
+		print("play card for target(s) ", card_ui.targets)
+		
+func on_input(_event: InputEvent) -> void:
+	if played:
+		return
+		
+	transition_requested.emit(self, CardState.State.BASE)
